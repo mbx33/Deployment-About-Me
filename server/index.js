5590e2d0a0e55ef3c2651315d5e88f8d09259973
Matt Baxter <mbx33@hotmail.com>
2021-08-12 12:08:01 -0600

    Initial

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//This is used when deploying an app 
+app.get('/', function (req, res ) {
+    res.sendFile(path.join(__dirname, '../frontEnd/index.html'))
+});
+
+
+const port = process.env.PORT || 5050;
+
+app.listen(port, () => {
+    console.log(`Ya Rabotayu on ${port}`)
+});
